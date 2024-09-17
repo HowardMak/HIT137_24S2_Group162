a7783dc74de03ff8e473590d47f2f99d5e236340
HowardMak <howie1113@gmail.com>
2024-09-17 18:17:32 +0930

    Reviewed by Howard

--- A2/Q3_Solved_By_Siddharth/solution_by_Siddharth.py
+++ A2/Q3_Solved_By_Siddharth/solution_by_Siddharth.py
@@ -85,7 +85,7 @@ print(original_code)
 print("Decrypted Code")
 print(decrypted_code)
 
-Final_Code = """
+decrypted_code = """
 global_variable = 100
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
@@ -97,48 +97,29 @@ def process_numbers():
         if local_variable % 2 == 0:
             numbers.remove(local_variable)
         local_variable -= 1
-        
-#Using The provided Logic codes
-
-    total = 0
-    for i in range(5):
-        for j in range(3):
-            if i + j == 5:
-                total += i + j
-            else:
-                total -= i - j
-
-    counter = 0
-    while counter < 5:
-        if total < 13:
-            total += 1
-        elif total > 13:
-            total -= 1
-        else:
-            counter += 2
-
     return numbers
 
 my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
-result = process_numbers()
+result = process_numbers(numbers=my_set)
 
 def modify_dict():
     local_variable = 10
     my_dict['key4'] = local_variable
 
-modify_dict()
+modify_dict(5)
 
-def update_global():
+def upbate_global():
     global global_variable
     global_variable += 10
 
 for i in range(5):
     print(i)
+    i += 1
 
-if my_set is not None and my_dict['key4'] == 10:
+if my_set is not None and my_dict['ke14'] == 10:
     print("Condition met!")
 
-if 'key5' not in my_dict:
+if 5 not in my_dict:
     print("5 not found in the dictionary!")
 
 print(global_variable)
@@ -146,6 +127,51 @@ print(my_dict)
 print(my_set)
 """
 
-print("Running Final Code!")
-exec(Final_Code)
+corrected_code = """
+global_variable = 100
+my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+
+# TypeError: process_numbers() got an unexpected keyword argument 'numbers'
+def process_numbers(numbers): # Added a variable here to address the TypeError
+    global global_variable
+    local_variable = 5
+    numbers = [1, 2, 3, 4, 5]
+    while local_variable > 0:
+        if local_variable % 2 == 0:
+            numbers.remove(local_variable)
+        local_variable -= 1
+    return numbers
+
+my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+result = process_numbers(numbers=my_set)
+
+# TypeError: modify_dict() takes 0 positional arguments but 1 was given
+def modify_dict():
+    local_variable = 10
+    my_dict['key4'] = local_variable
+
+modify_dict() # Removed the parameter to address the TypeError
+
+def upbate_global():
+    global global_variable
+    global_variable += 10
+
+for i in range(5):
+    print(i)
+    i += 1
+
+# KeyError: 'ke14'
+if my_set is not None and my_dict['key4'] == 10: # CHanged `ke14` to `key4` to address KeyError
+    print("Condition met!")
+
+if 5 not in my_dict:
+    print("5 not found in the dictionary!")
+
+print(global_variable)
+print(my_dict)
+print(my_set)
+"""
+
+print("Running Corrected Code!")
+exec(corrected_code)
 print("Done!")
